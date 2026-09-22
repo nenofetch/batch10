3d1f1f8ab951dd9d97e825b95bd2789424a19afe
Neno Arisma <22952560+nenofetch@users.noreply.github.com>
2026-09-22 20:13:27 +0700

    update pembelajaran

---
+++ tugas3/assets/js/script.js
@@ -0,0 +1,5 @@
+if (window.location.href.includes("projects.html")) {
+    document.getElementsByTagName("a")[3].classList.add("active");
+} else if (window.location.href.includes("bootstrapcss.html")) {
+    document.getElementsByTagName("a")[0].classList.add("active");
+}
